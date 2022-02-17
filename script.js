874bb21e0509bf7c2a894f0f24f1360a0f18458c
Fuad Saneen Parakkal <para0091@algonquinlive.com>
2022-02-16 21:31:02 -0500

    created interactive components & event delegation

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 
+// Targeting elements
 const $images = document.getElementById('images')
+const $description = document.getElementById('description')
 
-const images = []
+const imageGallery = []
 
-for (let i=1; i<11; i++) {
-    images.push(`<div id="image-container">
-    <img src="images/${i}.jpg" class="image">
-    <button class="more">Learn More</button>
-    </div>`)
+// Generate the image by incrementing id by each time
+for (let i = 1; i < 11;i++){
+    
+    imageGallery.push(` 
+        <div class='image' id='image${i}'>
+        <div>
+            <a href="#"><img src='images/${i}.jpg' class="each-img"></a>
+            <h3>Photo #${i}</h3>           
+        </div>
+        </div>
+    `)
 }
 
-$images.innerHTML += images.join('')
+$images.innerHTML = imageGallery.join('')
+
+const $imageList = document.querySelectorAll('.image')
+
+for (const image of $imageList){
+        
+    image.children[0].addEventListener('click', function(e) {
+       
+    $images.innerHTML = image.children[0].innerHTML + `<button class='back-button' onclick="location.reload()">Go Back</button>`
+    
+    $images.id = "big-image"
+    
+    })
+}
 
 
 
+
+
+
+
+
+
+
+ 
